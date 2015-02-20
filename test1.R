1140a067a91f3a5fd91bc260366ba865621ac997
JJ Chua <jjchua@users.noreply.github.com>
2015-02-20 18:31:46 +1100

    additional test cases

--- test1.R
+++ test1.R
@@ -3,21 +3,42 @@ library("testit")
 source("cachematrix.R")
 
 # sample x and its inverse for testing
-xraw <- matrix(1:4, ncol=2)
-expected <- matrix(c(-2,1,1.5,-0.5), ncol=2)
+y1 <- matrix(1:4, ncol=2)
+y1inv <- matrix(c(-2,1,1.5,-0.5), ncol=2)
+y2 <- matrix(c(7:10), ncol=2)
+y2inv <- matrix(c(-5,4,4.5,-3.5), ncol=2)
 
 # test the constructor
-x <- makeCacheMatrix(xraw)
-assert("set/get", all.equal(xraw, x$get()))
+x <- makeCacheMatrix(y1)
+assert("set/get", all.equal(y1, x$get()))
 assert("empty cache", is.null(x$getinverse()))
 
 # test that the inverse is calculated correctly
 xinv <- cacheSolve(x)
-assert("inversion", all.equal(expected, xinv))
+assert("inversion", all.equal(y1inv, xinv))
 
 # test the cache
-assert("setinverse/getinverse", all.equal(expected, x$getinverse()))
+assert("setinverse/getinverse", all.equal(y1inv, x$getinverse()))
 
 # test that the cached is being used
 # should output "getting cached inverse\n" to stderr
 xinv <- cacheSolve(x)
+assert("correct cache", all.equal(y1inv, xinv))
+
+# test that the inverse is reset if x is changed
+x$set(y2)
+assert("x reset", all.equal(y2, x$get()))
+assert("xinv reset", is.null(x$getinverse()))
+
+# test that the inverse is re-calculated correctly
+xinv <- cacheSolve(x)
+assert("inversion", all.equal(y2inv, xinv))
+
+# test the cache
+assert("setinverse/getinverse", all.equal(y2inv, x$getinverse()))
+
+# test that the cached is being used
+# should output "getting cached inverse\n" to stderr
+xinv <- cacheSolve(x)
+assert("correct cache", all.equal(y2inv, xinv))
+
